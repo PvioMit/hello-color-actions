96e8a58e6f8b1f8e7f54a704336b00360c819104
PvioMit <156957626+PvioMit@users.noreply.github.com>
2024-01-22 13:18:02 -0300

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,3 +1,3 @@
-const hello = () => "Hello World from the Office Hours in the terminal!";
+const hello = () => "Hello, mundo, desde Espagna, Saludos Patricio Vio";
 console.log(hello());
 exports.hello = hello;
